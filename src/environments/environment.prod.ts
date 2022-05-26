9c71514ba550d2cdb835c1d55d9d635e8eb9d914
13Aashishgit <er.13aashishgupta@gmail.com>
2022-05-26 12:06:30 +0530

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  weatherApiBaseUrl:'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'bf6922cdb5msh575d46e71686f32p1c1414jsn2d1f08edd5d9'
 };
